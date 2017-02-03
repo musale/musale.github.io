1c2975a4b6871e8389f5b0dc1ad6c0765d7281ec
musale <musale@focusmobile.com>
2017-02-03 10:54:55 +0300

    :thumbsup: added jumbotron to the homepage and info

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 var React = require('react');
 
-function Home (props) {
-  return (
-    <div >
-      Home
-    </div>
-  )
+function Home(props) {
+    return (
+        <div className="container">
+            <div className="jumbotron">
+                <h1>Martin Musale Github Page</h1>
+                <h4> This page is still under construction.Check back later or reach me at <code> martinmshale@gmail.com </code></h4 >
+            </div>
+        </div>
+    )
 }
 
 module.exports = Home;
