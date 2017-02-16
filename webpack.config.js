fbd0f0e78e17e69374c85689a7acdb8239d3bad8
Musale Martin <martinmshale@gmail.com>
2017-02-16 19:16:47 +0300

    load css filess using loaders

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports =  {
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
+      {test: /\.css$/, loader: "style-loader!css-loader"},
     ],
   },
 
