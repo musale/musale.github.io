7a10dc97fc4b6794dc8a8f629fb6029fea15cb0e
Musale Martin <martinmshale@gmail.com>
2017-02-16 19:16:54 +0300

    new files

---
+++ src/styles/baseStyle.js
@@ -0,0 +1,15 @@
+var styles = {
+    main: {
+        width: "100%",
+        height: "100%"
+    },
+    background: {
+        backgroundSize: 'cover',
+        backgroundImage: "url('src/images/background.jpg')",
+        display: 'flex',
+        height: '100%',
+        width: '100%'
+    }
+}
+
+module.exports = styles;
