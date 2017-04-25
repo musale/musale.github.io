92e0df9cbcbb1d61fdaab485aa73b14af4a66ba1
musale <martinmshale@gmail.com>
2017-04-25 13:57:58 +0300

    Updated main Service Worker.

--- sw.js
+++ sw.js
@@ -1,77 +1,54 @@
-const CACHE_NAME = 'neutron-v1';
-const urlsToCache = [
-  '/index.html',
+const cacheVersion = '1.0.0';
+const staticCacheName = `musale-v${cacheVersion}`;
+const filesToCache = [
+  './',
+  '/(index)',
   '/styles/main.css',
   '/styles/bootstrap.min.css',
   '/images/3246305.jpg',
-  '/index.19fb4943df0cf427704a.bundle.js',
-  '/manifest.1487ea393e348f6eea30.bundle.js',
+  '/images/001.svg',
+  '/images/002.png',
+  '/images/003.png',
+  '/images/004.jpg',
+  '/images/005.jpg',
+  '/index.bundle.js',
+  '/manifest.bundle.js',
   '/scripts/loadsw.js',
   '/scripts/bootstrap.min.js',
   '/scripts/jquery-3.1.1.slim.min.js',
   '/scripts/tether.min.js',
+  'https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg',
+  'https://fonts.gstatic.com/s/raleway/v11/yQiAaD56cjx1AooMTSghGfY6323mHUZFJMgTvxaG2iE.woff2',
+  'https://fonts.gstatic.com/s/raleway/v11/0dTEPzkLWceF7z0koJaX1A.woff2',
+  'https://placehold.it/355x250',
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  console.log('Installing SW!');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+this.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(staticCacheName).then(function(cache) {
+    return cache.addAll(filesToCache);
+  }));
 });
 
-this.addEventListener('activate', function(event) {
-  const cacheWhitelist = [CACHE_NAME];
 
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Clone the request. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // We want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('musale-v') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
