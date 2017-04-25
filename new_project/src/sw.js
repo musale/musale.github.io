1f7e0be2f28fa4a69fa0971b171a0860652e4f7d
musale <martinmshale@gmail.com>
2017-04-25 13:54:57 +0300

    Updated service worker.

--- new_project/src/sw.js
+++ new_project/src/sw.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = 'neutron-v3';
-const urlsToCache = [
+const cacheVersion = '1.0.0';
+const staticCacheName = `musale-v${cacheVersion}`;
+const filesToCache = [
   './',
   '/(index)',
   '/styles/main.css',
@@ -22,66 +23,32 @@ const urlsToCache = [
   'https://placehold.it/355x250',
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  console.log('Installing SW!');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+this.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(staticCacheName).then(function(cache) {
+    return cache.addAll(filesToCache);
+  }));
 });
 
-this.addEventListener('activate', function(event) {
-  const cacheWhitelist = [CACHE_NAME];
 
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Clone the request. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // We want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('musale-v') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
